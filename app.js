ecf83099301c75d936dd71e30bbe1ccd0d57cab9
Yashwanth B K <132933255+Yashwanth-Gowda-BK@users.noreply.github.com>
2024-01-20 23:06:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static('./public'));
 app.use(express.json());
 app.use('/api/vehicle/cars',car)
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async ()=>{
     try {
